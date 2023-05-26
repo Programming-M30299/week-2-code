091c1b72fce9b4214832cbefe311ca8c1f0c343c
manighahrmani <mashapicasso@gmail.com>
2023-05-27 00:48:26 +0100

    Update pract2.py

--- pract2.py
+++ pract2.py
@@ -1,12 +1,12 @@
-from math import *
+import math
 
 
 def heightOfTriangle():
     base = float(input("Enter base: "))
     angleInDegrees = float(input("Enter angle: "))
 
-    angleInRadians = radians(angleInDegrees)
-    height = base * tan(angleInRadians)
+    angleInRadians = math.radians(angleInDegrees)
+    height = base * math.tan(angleInRadians)
     print("Height of triangle: ", round(height, 2))
 
 
@@ -16,7 +16,7 @@ def distanceBetweenPoints():
     x2 = float(input("Enter x2: "))
     y2 = float(input("Enter y2: "))
 
-    distance = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
     print("Distance between points: ", round(distance, 2))
 
 # TODO: Add your solutions to the programming exercises here.
