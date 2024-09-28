825549615aa2cc752e6e87efdd3aead76f60e721
manighahrmani <mashapicasso@gmail.com>
2024-09-29 00:36:58 +0100

    Update all to use snake_case

--- pract2.py
+++ pract2.py
@@ -1,23 +1,20 @@
 import math
 
-
-def squareRootCalculator():
+def square_root_calculator():
     num = float(input("Enter a non-negative number: "))
     if num < 0:
         print("Square root is undefined for negative numbers.")
     else:
-        sqrtValue = math.sqrt(num)
-        print("Square root:", round(sqrtValue, 2))
+        sqrt_value = math.sqrt(num)
+        print("Square root:", round(sqrt_value, 2))
 
-
-def sineAndCosine():
+def sine_and_cosine():
     angle = float(input("Enter an angle in degrees: "))
-    angleInRadians = math.radians(angle)  # converts degrees to radians
-    sineValue = math.sin(angleInRadians)
-    cosineValue = math.cos(angleInRadians)
-    print("Sine of the angle:", round(sineValue, 2))
-    print("Cosine of the angle:", round(cosineValue, 2))
-
+    angle_in_radians = math.radians(angle)  # converts degrees to radians
+    sine_value = math.sin(angle_in_radians)
+    cosine_value = math.cos(angle_in_radians)
+    print("Sine of the angle:", round(sine_value, 2))
+    print("Cosine of the angle:", round(cosine_value, 2))
 
 # Add your solutions to the programming exercises here.
 # Remember that the `math` module is already imported for you (do not import it again!)
